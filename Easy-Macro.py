13f31409ab7722337a3be614f7e926099c2537da
Diego Romero <diego.romero.s@hotmail.com>
2022-11-16 03:12:06 -0300

    Easy-Macro

---
+++ Easy-Macro.py
@@ -0,0 +1,30 @@
+import pyautogui 
+import time
+
+for i in range(300):
+
+    #Find Opponent
+    pyautogui.click(x=426, y=934)
+    pyautogui.click(x=426, y=411)
+    pyautogui.click(x=1570, y=272)
+    pyautogui.click(x=1570, y=607)
+    pyautogui.click(x=1570, y=939)
+    time.sleep(3)
+    #Fight!
+    pyautogui.click(x=608, y=341)
+    pyautogui.click(x=608, y=866)
+    pyautogui.click(x=1700, y=224)
+    pyautogui.click(x=1700, y=561)
+    pyautogui.click(x=1700, y=893)
+    time.sleep(4.5)
+    #Collect
+    pyautogui.click(x=426, y=448)
+    pyautogui.click(x=426, y=975)
+    pyautogui.click(x=1570, y=292)
+    pyautogui.click(x=1570, y=627)
+    pyautogui.click(x=1570, y=962)
+    time.sleep(1)
+
+
+
+        
