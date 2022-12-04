7f921298752e26f6609c1d3f503b5043be15c2de
Diego Romero <diego.romero.s@hotmail.com>
2022-12-04 03:00:04 -0300

    Advance macro Working

--- Macro-notWorking.py
+++ Macro-notWorking.py
@@ -1,59 +1,102 @@
-import pyautogui 
-import time
 
-minirana = pyautogui.locateOnScreen('minirana.png', confidence=0.8)
-darkcloud = pyautogui.locateOnScreen('darkcloud.png',confidence=0.8, grayscale = True )
-diego1000 = pyautogui.locateOnScreen('diego1000.png',confidence=0.8, grayscale = True )
-diego1001 = pyautogui.locateOnScreen('diego1001.png',confidence=0.8, grayscale = True )
-diego1002 = pyautogui.locateOnScreen('diego1002.png',confidence=0.8, grayscale = True )
+"""
+import cv2
+import numpy as np
 
-for i in range(300):
+diego1001 = cv2.imread('diego1001.png', cv2.IMREAD_UNCHANGED)
+findOpponent = cv2.imread('findOpponent.png', cv2.IMREAD_UNCHANGED)
+desktop = cv2.imread('desktop-5-Screen.png', cv2.IMREAD_UNCHANGED)
 
-    #Find Opponent
-    pyautogui.click(x=426, y=934)
-    pyautogui.click(x=426, y=411)
-    pyautogui.click(x=1570, y=272)
-    pyautogui.click(x=1570, y=607)
-    pyautogui.click(x=1570, y=939)
-    time.sleep(3)
-    #Fight!
-    pyautogui.click(x=608, y=341)
-    pyautogui.click(x=608, y=866)
-    pyautogui.click(x=1700, y=224)
-    pyautogui.click(x=1700, y=561)
-    pyautogui.click(x=1700, y=893)
-    time.sleep(4.5)
-    #Collect
-    pyautogui.click(x=426, y=448)
-    pyautogui.click(x=426, y=975)
-    pyautogui.click(x=1570, y=292)
-    pyautogui.click(x=1570, y=627)
-    pyautogui.click(x=1570, y=962)
-    time.sleep(2)
+result = cv2.matchTemplate(desktop, findOpponent, cv2.TM_CCOEFF_NORMED)
 
-    if minirana == None:
-        pyautogui.click(x=1895, y=261)
-        xyz = pyautogui.locateOnScreen('dragonary.png',  confidence=0.8, grayscale = True )
-        pyautogui.click(xyz)
-    elif darkcloud == None:
-        pyautogui.click(x=1895, y=925)
-        xyz = pyautogui.locateOnScreen('dragonary.png',  confidence=0.8, grayscale = True )
-        pyautogui.click(xyz)
-    elif diego1000 == None:
-        pyautogui.click(x=875, y=960)
-        xyz = pyautogui.locateOnScreen('dragonary.png',  confidence=0.8, grayscale = True )
-        pyautogui.click(xyz)
-    elif diego1001 == None:
-        pyautogui.click(x=880, y=432)
-        xyz = pyautogui.locateOnScreen('dragonary.png',  confidence=0.8, grayscale = True )
-        pyautogui.click(xyz)
-    elif diego1002 == None:
-        pyautogui.click(x=1895, y=591)
-        xyz = pyautogui.locateOnScreen('dragonary.png',  confidence=0.8, grayscale = True )
-        pyautogui.click(xyz)
-    
-
-    
+w = findOpponent.shape[1]
+h = findOpponent.shape[0]
 
 
-        
+
+min_val, max_val, min_loc, max_loc =  cv2.minMaxLoc(result)
+squareBox = cv2.rectangle(desktop, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255),2)
+
+
+print(max_loc)
+print(max_val)
+
+threshold = .60
+yloc, xloc = np.where(result >= threshold)
+len(xloc)
+for(x,y) in zip(xloc,yloc):
+    cv2.rectangle(desktop, (x,y), (x + w, y+ h),(0,255,255),2)
+
+result = cv2.matchTemplate(desktop, findOpponent, cv2.TM_CCOEFF_NORMED)
+
+
+cv2.imshow('Desktop', desktop)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+"""
+
+import cv2 
+import numpy as np
+
+minirana = cv2.imread('minirana.png', cv2.IMREAD_UNCHANGED)
+darkcloud = cv2.imread('darkcloud.png', cv2.IMREAD_UNCHANGED)
+diego1000 = cv2.imread('diego1000.png', cv2.IMREAD_UNCHANGED)
+diego1001 = cv2.imread('diego1001.png', cv2.IMREAD_UNCHANGED)
+diego1002 = cv2.imread('diego1002.png', cv2.IMREAD_UNCHANGED)
+findOpponent = cv2.imread('findOpponent.png', cv2.IMREAD_UNCHANGED)
+desktop = cv2.imread('desktop3.png', cv2.IMREAD_UNCHANGED)
+
+
+result = cv2.matchTemplate(desktop, minirana, cv2.TM_CCOEFF_NORMED)
+result2 = cv2.matchTemplate(desktop, darkcloud, cv2.TM_CCOEFF_NORMED)
+result3 = cv2.matchTemplate(desktop, diego1000, cv2.TM_CCOEFF_NORMED)
+result4 = cv2.matchTemplate(desktop, diego1001, cv2.TM_CCOEFF_NORMED)
+result5 = cv2.matchTemplate(desktop, diego1002, cv2.TM_CCOEFF_NORMED)
+
+
+min_val, max_val, min_loc, max_loc =  cv2.minMaxLoc(result)
+min_val2, max_val2, min_loc2, max_loc2 =  cv2.minMaxLoc(result2)
+min_val3, max_val3, min_loc3, max_loc3 =  cv2.minMaxLoc(result3)
+min_val4, max_val4, min_loc4, max_loc4 =  cv2.minMaxLoc(result4)
+min_val5, max_val5, min_loc5, max_loc5 =  cv2.minMaxLoc(result5)
+
+print(min_val)
+print(max_val)
+print(min_val2)
+print(max_val2)
+print(min_val3)
+print(max_val3)
+print(min_val4)
+print(max_val4)
+print(min_val5)
+print(max_val5)
+
+w = minirana.shape[1]
+h = minirana.shape[0]
+
+w2 = darkcloud.shape[1]
+h2 = darkcloud.shape[0]
+
+w3 = diego1000.shape[1]
+h3 = diego1000.shape[0]
+
+w4 = diego1001.shape[1]
+h4 = diego1001.shape[0]
+
+w5 = diego1002.shape[1]
+h5 = diego1002.shape[0]
+
+squareBox = cv2.rectangle(desktop, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255),2)
+squareBox2 = cv2.rectangle(desktop, max_loc2, (max_loc2[0] + w2, max_loc2[1] + h2), (0,255,255),2)
+squareBox3 = cv2.rectangle(desktop, max_loc3, (max_loc3[0] + w3, max_loc3[1] + h3), (0,255,255),2)
+squareBox4 = cv2.rectangle(desktop, max_loc4, (max_loc4[0] + w4, max_loc4[1] + h4), (0,255,255),2)
+squareBox5 = cv2.rectangle(desktop, max_loc5, (max_loc5[0] + w5, max_loc5[1] + h5), (0,255,255),2)
+
+
+
+cv2.imshow('Desktop', desktop)
+
+
+
---
+++ Max_Macro(i wish!!).py
@@ -0,0 +1,164 @@
+import cv2 as cv
+import numpy as np
+import pyautogui
+import time
+
+minirana = cv.imread('minirana_2.png', cv.IMREAD_UNCHANGED)
+darkcloud = cv.imread('darkcloud_2.png', cv.IMREAD_UNCHANGED)
+diego1000 = cv.imread('diego1000_2.png', cv.IMREAD_UNCHANGED)
+diego1001 = cv.imread('diego1001_2.png', cv.IMREAD_UNCHANGED)
+diego1002 = cv.imread('diego1002_2.png', cv.IMREAD_UNCHANGED)
+
+desktop =  pyautogui.screenshot() #cv2.imread('desktop3.png', cv2.IMREAD_UNCHANGED)
+desktop = cv.cvtColor(np.array(desktop), cv.COLOR_RGB2BGR)
+
+
+cv.imwrite("in_memory_to_disk.png", desktop)
+
+resultS1 = cv.matchTemplate(desktop, minirana, cv.TM_CCOEFF_NORMED)
+resultS2 = cv.matchTemplate(desktop, darkcloud, cv.TM_CCOEFF_NORMED)
+resultS3 = cv.matchTemplate(desktop, diego1000, cv.TM_CCOEFF_NORMED)
+resultS4 = cv.matchTemplate(desktop, diego1001, cv.TM_CCOEFF_NORMED)
+resultS5 = cv.matchTemplate(desktop, diego1002, cv.TM_CCOEFF_NORMED)
+
+min_valS1, max_valS1, min_locS1, max_locS1 = cv.minMaxLoc(resultS1)
+min_valS2, max_valS2, min_locS2, max_locS2 = cv.minMaxLoc(resultS2)
+min_valS3, max_valS3, min_locS3, max_locS3 = cv.minMaxLoc(resultS3)
+min_valS4, max_valS4, min_locS4, max_locS4 = cv.minMaxLoc(resultS4)
+min_valS5, max_valS5, min_locS5, max_locS5 = cv.minMaxLoc(resultS5)
+
+threshold = 0.8
+
+
+    
+def clicker(S1,S2,S3,S4,S5):
+    #Find Opponent
+    pyautogui.click(x=390, y=400)
+    pyautogui.click(x=390, y=887)
+    pyautogui.click(x=1570, y=284)
+    pyautogui.click(x=1570, y=636)
+    pyautogui.click(x=1570, y=975)
+    time.sleep(3)
+    #Fight!
+    pyautogui.click(x=608, y=326)
+    pyautogui.click(x=608, y=818)
+    pyautogui.click(x=1700, y=234)
+    pyautogui.click(x=1700, y=588)
+    pyautogui.click(x=1700, y=920)
+    time.sleep(4.5)
+    #Collect
+    pyautogui.click(x=400, y=425)
+    pyautogui.click(x=400, y=910)
+    pyautogui.click(x=1560, y=308)
+    pyautogui.click(x=1560, y=657)
+    pyautogui.click(x=1560, y=998)
+    
+    if S1 == 1:
+        pyautogui.click(x=1900, y=618)
+        time.sleep(1)
+        pyautogui.click(x=1780, y=476)
+    elif S2 == 1:
+        pyautogui.click(x=1895, y=965)
+        time.sleep(1)
+        pyautogui.click(x=1774, y=817)
+    elif S3 == 1:
+        pyautogui.click(x=827, y=894)
+        time.sleep(1)
+        pyautogui.click(x=680, y=640)
+    elif S4 == 1:
+        pyautogui.click(x=826, y=405)
+        time.sleep(1)
+        pyautogui.click(x=680, y=147)
+    elif S5 == 1:
+        pyautogui.click(x=1896, y=277)
+        time.sleep(1)
+        pyautogui.click(x=1770, y=127)
+
+        
+for i in range(300):
+
+    minirana = cv.imread('minirana_2.png', cv.IMREAD_UNCHANGED)
+    darkcloud = cv.imread('darkcloud_2.png', cv.IMREAD_UNCHANGED)
+    diego1000 = cv.imread('diego1000_2.png', cv.IMREAD_UNCHANGED)
+    diego1001 = cv.imread('diego1001_2.png', cv.IMREAD_UNCHANGED)
+    diego1002 = cv.imread('diego1002_2.png', cv.IMREAD_UNCHANGED)
+
+    desktop =  pyautogui.screenshot() #cv2.imread('desktop3.png', cv2.IMREAD_UNCHANGED)
+    desktop = cv.cvtColor(np.array(desktop), cv.COLOR_RGB2BGR)
+
+
+    cv.imwrite("in_memory_to_disk.png", desktop)
+
+    resultS1 = cv.matchTemplate(desktop, minirana, cv.TM_CCOEFF_NORMED)
+    resultS2 = cv.matchTemplate(desktop, darkcloud, cv.TM_CCOEFF_NORMED)
+    resultS3 = cv.matchTemplate(desktop, diego1000, cv.TM_CCOEFF_NORMED)
+    resultS4 = cv.matchTemplate(desktop, diego1001, cv.TM_CCOEFF_NORMED)
+    resultS5 = cv.matchTemplate(desktop, diego1002, cv.TM_CCOEFF_NORMED)
+
+    min_valS1, max_valS1, min_locS1, max_locS1 = cv.minMaxLoc(resultS1)
+    min_valS2, max_valS2, min_locS2, max_locS2 = cv.minMaxLoc(resultS2)
+    min_valS3, max_valS3, min_locS3, max_locS3 = cv.minMaxLoc(resultS3)
+    min_valS4, max_valS4, min_locS4, max_locS4 = cv.minMaxLoc(resultS4)
+    min_valS5, max_valS5, min_locS5, max_locS5 = cv.minMaxLoc(resultS5)
+
+    threshold = 0.8
+
+
+
+
+    if max_valS1 >= threshold:
+        print('found match')
+        S1 = 0
+    else:
+        print('not found match')
+        S1 = 1
+    if max_valS2 >= threshold:
+        print('found match')
+        S2 = 0
+    else:
+        print('not found match')
+        S2 = 1
+
+    if max_valS3 >= threshold:
+        print('found match')
+        S3 = 0
+
+    else:
+        print('not found match')
+        S3 = 1
+
+    if max_valS4 >= threshold:
+        print('found match')
+        S4 = 0
+
+    else:
+        print('not found match')
+        S4 = 1
+
+    if max_valS5 >= threshold:
+        print('found match')
+        S5 = 0
+
+    else:
+        print('not found match')
+        S5 = 1
+    clicker(S1,S2,S3,S4,S5)
+    
+    desktop =  pyautogui.screenshot() #cv2.imread('desktop3.png', cv2.IMREAD_UNCHANGED)
+    desktop = cv.cvtColor(np.array(desktop), cv.COLOR_RGB2BGR)
+
+
+    cv.imwrite("in_memory_to_disk.png", desktop)
+
+    resultS1 = cv.matchTemplate(desktop, minirana, cv.TM_CCOEFF_NORMED)
+    resultS2 = cv.matchTemplate(desktop, darkcloud, cv.TM_CCOEFF_NORMED)
+    resultS3 = cv.matchTemplate(desktop, diego1000, cv.TM_CCOEFF_NORMED)
+    resultS4 = cv.matchTemplate(desktop, diego1001, cv.TM_CCOEFF_NORMED)
+    resultS5 = cv.matchTemplate(desktop, diego1002, cv.TM_CCOEFF_NORMED)
+
+    min_valS1, max_valS1, min_locS1, max_locS1 = cv.minMaxLoc(resultS1)
+    min_valS2, max_valS2, min_locS2, max_locS2 = cv.minMaxLoc(resultS2)
+    min_valS3, max_valS3, min_locS3, max_locS3 = cv.minMaxLoc(resultS3)
+    min_valS4, max_valS4, min_locS4, max_locS4 = cv.minMaxLoc(resultS4)
+    min_valS5, max_valS5, min_locS5, max_locS5 = cv.minMaxLoc(resultS5)
+
